66b66047b4d4cb53e61769fc3edc6ca385cc9d18
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-11 15:14:48 -0600

    updated fav page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -55,11 +55,15 @@ router.get("/signup", (req, res) => {
 // Route to render individual recipe page
 router.get("/recipe/:id", withAuth, async (req, res) => {
 
-    const params = req.params.id.split("_");
-    const id = params[0];
+    const id = req.params.id;
     let favorite = false;
-    if (params.length > 1) {
-        favorite = true;
+    const recipeData = await Recipe.findOne({where: {spoon_id: id}});
+    if (recipeData) {
+        const recipe = recipeData.get({plain: true});
+        const favoriteData = await Favorite.findOne({where: {recipe_id: recipe.id, user_id: req.session.user_id}});
+        if (favoriteData) {
+            favorite= true;
+        }
     }
 
     const instructionsURL = `https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=${API_KEY}`
@@ -95,103 +99,5 @@ router.get("/recipe/:id", withAuth, async (req, res) => {
         logged_in: req.session.logged_in,
         favorite: favorite
     });
-
-    /*
-     try {
-         // Find recipe by ID with associated username and comments with associated usernames
-         const recipeData = await recipe.findByPk(req.params.id, {
-             include: [{
-                 model: User,
-                 attributes: ["username"]
-             },
-             {
-                 model: Comment,
-                 include: [{
-                     model: User,
-                     attributes: ["username"]
-                 }],
-             },
-             ],
-         });
-         // Convert recipe data to plain JavaScript object and render recipe templates with recipe data and login status
-         const recipe = recipeData.get({
-             plain: true
-         });
-         res.render("recipe", {
-             ...recipe,
-             logged_in: req.session.logged_in,
-         });
-     } catch (err) {
-         res.status(500).json(err);
-     }*/
 });
-
-/*// Route to render dashboard page with all recipes by current user then finding all recipes by current user with associated usernames
-router.get("/dashboard", withAuth, async (req, res) => {
-    try {
-        const recipeData = await recipe.findAll({
-            where: {
-                user_id: req.session.user_id
-            },
-            include: [{
-                model: User,
-                attributes: ["username"]
-            }],
-        });
-        // Convert recipe data to plain JavaScript object
-        const recipes = recipeData.map((recipe) => recipe.get({
-            plain: true
-        }));
-
-        res.render("dashboard", {
-            recipes,
-            logged_in: req.session.logged_in,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-//render the new recipe page
-router.get("/newrecipe", (req, res) => {
-    if (req.session.logged_in) {
-        res.render("newrecipe");
-        return;
-    }
-    res.redirect("/login");
-});
-
-
-//render the edit recipe page
-router.get("/editrecipe/:id", async (req, res) => {
-    try {
-        const recipeData = await recipe.findByPk(req.params.id, {
-            include: [{
-                model: User,
-                attributes: ["username"]
-            },
-            {
-                model: Comment,
-                include: [{
-                    model: User,
-                    attributes: ["username"]
-                }],
-            },
-            ],
-        });
-
-        const recipe = recipeData.get({
-            plain: true
-        });
-
-        res.render("editrecipe", {
-            ...recipe,
-            logged_in: req.session.logged_in,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-// module exports router
-*/
 module.exports = router;

c47e0e2c3a1a56b92a68fcaaafe179cd7e349172
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-11 15:22:03 -0600

    changed up home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -70,23 +70,22 @@ router.get("/recipe/:id", withAuth, async (req, res) => {
     const nutritionURL = `https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=${API_KEY}`
     const infoURL = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`;
 
-    const response_instructions = await axios.get(instructionsURL);
-    const instructions = response_instructions;
-    const response_nutritions = await axios.get(nutritionURL);
+    const instructions = await axios.get(instructionsURL);
+    const responseNutritions = await axios.get(nutritionURL);
 
 
-    const nutritions = response_nutritions.data;
+    const nutritions = responseNutritions.data;
 
-    const response_info = await axios.get(infoURL);
+    const responseInfo = await axios.get(infoURL);
     let instr = "";
     if (instructions.data.length > 0) {
         instr = instructions.data[0].steps;
     }
 
-    const title = response_info.data.title;
-    const servings = response_info.data.servings;
-    const readyInMinutes = response_info.data.readyInMinutes;
-    const imgSource = response_info.data.image;
+    const title = responseInfo.data.title;
+    const servings = responseInfo.data.servings;
+    const readyInMinutes = responseInfo.data.readyInMinutes;
+    const imgSource = responseInfo.data.image;
 
     const result = {
         title: title, servings: servings, readyInMinutes: readyInMinutes, imgSource: imgSource, instructions: instr, ingredients: nutritions.ingredients, nutrients:
